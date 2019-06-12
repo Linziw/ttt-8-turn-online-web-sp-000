8aeda71a474634415111336b661c6af4cfdf7441
Lindsey Wells <lindsey.wells84@gmail.com>
2019-06-12 05:32:31 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,13 +38,12 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
 if valid_move?(board, index)
-  move (board, index, char = "X")
+  move (board, index)
   display_board (board)
 else
   turn(board)
 end
 end
-
 
 
 
